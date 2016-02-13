92a73b09fae2ffd86f614e19dd83ec87cf62c4f9
b1015062Furukawa <b1015062@fun.ac.jp>
2016-02-14 08:56:28 +0900

    create runnable one
    just a very little prototype

--- src/Timer.js
+++ src/Timer.js
@@ -1,8 +1,10 @@
 var Timer = {}
 
+Timer._defaultms = 0;
 Timer._leftms = 0;
 Timer._isStarted = false;
 Timer._startedTime = 0;
+Timer._isFinished = false;
 
 Timer.start = function()
 {
@@ -11,7 +13,9 @@ Timer.start = function()
     }
 
     this._isStarted = true;
+    this._isFinished = false;
     this._startedTime = new Date().getTime();
+    this._defaultms = this._leftms;
 }
 
 Timer.stop = function()
@@ -22,23 +26,29 @@ Timer.stop = function()
 Timer.update = function()
 {
     if( this._isStarted ) {
-        this._leftms = new Date().getTime() - this._startedTime;
+        this._leftms = this._defaultms - (new Date().getTime() - this._startedTime);
+
+        if( this._leftms <= 0 ) {
+            this._isFinished = true;
+            this.stop();
+            this._leftms = 0;
+        }
     }
 }
 
 Timer.getHours = function()
 {
-    return this._leftms / (1000 * 60 * 60);
+    return Math.floor(this._leftms / (1000 * 60 * 60));
 }
 
 Timer.getMinutes = function()
 {
-    return this._leftms / (1000 * 60);
+    return Math.floor( (this._leftms - 1000 * 60 * 60 * this.getHours()) / (1000 * 60));
 }
 
 Timer.getSeconds = function()
 {
-    return this._leftms / 1000;
+    return Math.floor((this._leftms - 1000 * 60 * 60 * this.getHours() - 1000 * 60 * this.getMinutes() ) / 1000);
 }
 
 Timer.addHours = function(hours)
@@ -53,7 +63,7 @@ Timer.addMinutes = function(minutes)
 
 Timer.addSeconds = function(seconds)
 {
-    this.leftms += 1000 * seconds;
+    this._leftms += 1000 * seconds;
 }
 
 module.exports = Timer;
--- src/main.js
+++ src/main.js
@@ -1,1 +1,38 @@
-var Timer = require("./src/Timer.js");
+var Timer = null;
+var timerID = null;
+
+function zfill(number)
+{
+    if( number < 10 ) {
+        return 0 + String(number);
+    } else {
+        return number;
+    }
+}
+
+function start()
+{
+    Timer.start();
+    timerID = setInterval(update, 100)
+}
+
+function update()
+{
+    Timer.update();
+    $("span.hours").text(zfill(Timer.getHours()))
+    $("span.minutes").text(zfill(Timer.getMinutes()))
+    $("span.seconds").text(zfill(Timer.getSeconds()))
+}
+
+function stop()
+{
+    Timer.stop();
+    clearInterval(timerID);
+}
+
+window.onload = function()
+{
+    Timer = require("./src/Timer.js");
+    Timer.addSeconds(63);
+    update();
+}
