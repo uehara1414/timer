9535edd3ed87b9cae6085615c20232860f0040bf
b1015062Furukawa <b1015062@fun.ac.jp>
2016-02-15 09:57:54 +0900

    create second prototype
    ・サイズ変更に対応
    ・数値を画像に変更
    ・待ち時間を右クリック、左クリックで増減させられる
    ・ピンク色部分をドラッグで動かせる

--- src/main.js
+++ src/main.js
@@ -19,9 +19,20 @@ function start()
 function update()
 {
     Timer.update();
-    $("span.hours").text(zfill(Timer.getHours()))
-    $("span.minutes").text(zfill(Timer.getMinutes()))
-    $("span.seconds").text(zfill(Timer.getSeconds()))
+    display();
+}
+
+function display()
+{
+    var hours = String(zfill(Timer.getHours()));
+    var minutes = String(zfill(Timer.getMinutes()));
+    var seconds = String(zfill(Timer.getSeconds()));
+    $("img.hour1").attr("src", "images/gif34/" + hours[0] + ".gif");
+    $("img.hour2").attr("src", "images/gif34/" + hours[1] + ".gif");
+    $("img.minute1").attr("src", "images/gif34/" + minutes[0] + ".gif");
+    $("img.minute2").attr("src", "images/gif34/" + minutes[1] + ".gif");
+    $("img.second1").attr("src", "images/gif34/" + seconds[0] + ".gif");
+    $("img.second2").attr("src", "images/gif34/" + seconds[1] + ".gif");    
 }
 
 function stop()
@@ -30,9 +41,38 @@ function stop()
     clearInterval(timerID);
 }
 
+
 window.onload = function()
 {
+    $("div.secondsbox").mousedown(function(e){
+        if ( e.button == 2 ) {
+            Timer.addSeconds(-1);
+        } else {
+            Timer.addSeconds(1);
+        }
+        update();
+    });
+
+    $("div.minutesbox").mousedown(function(e){
+        if ( e.button == 2 ) {
+            Timer.addMinutes(-1);
+        } else {
+            Timer.addMinutes(1);
+        }
+        update();
+    });
+
+    $("div.hoursbox").mousedown(function(e){
+        if ( e.button == 2 ) {
+            Timer.addHours(-1);
+        } else {
+            Timer.addHours(1);
+        }
+        update();
+    });
+
     Timer = require("./src/Timer.js");
     Timer.addSeconds(63);
     update();
+    
 }
